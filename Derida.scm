7616ce250a531a4003624eaa8645bfb8ac457779
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-01-26 20:06:09 +0200

    Create Derida.scm

---
+++ Derida.scm
@@ -0,0 +1,12 @@
+(string=? "I am a copy of a copy" "I am a copy of a copy")  ;; => #t
+(string=? "-+-=0" "-+-=0");; => #t
+(string=? "Space is empty, but fun" "Space is empty but fun")                    ;; => #t
+(string=? "Experimentation with gravity does miracles" "Experimentation with gravity does miracles")     
+(string=? "Life on earth is simple but fun"  "Life on earth is simple but fun")
+(string=? "If there is nothing outside the text like Derida says then simple there is nothing"  "If there is nothing outside the text like Derida says then simple there is nothing") 
+(string=? "so reading on love or hate makes people slim"  "so reading on love or hate makes people slim") 
+(string=? "So many people do read"  "So many people do read")
+(string=? "the same fashion they write"  "the same fashion they write") 
+(string=? "Death is nothing to us, as Epicuros would say, but Max Tegmark gives a new perspective out of this.Why dont we make something out of nothing?" "Death is nothing to us, as Epicuros would say, but Max Tegmark gives a new perspective out of this.Why dont we make something out of nothing?")
+(string=? "This is a hell of advantage for social media"  "This is a hell of advantage for social media")
+(string=? "So many people agree on this very same idea that nature has no knowledge of depression"  "So many people agree on this very same idea that nature has no knowledge of depression")
